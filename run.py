870f976181f7b3cc500ef8d07834faf31f1db3ec
nualagr <nualagreenwood@gmail.com>
2020-11-04 11:20:42 +0000

    fix: moved half of line 43 to 44 after an opening bracket to get rid of the 'line too long' error.

--- run.py
+++ run.py
@@ -40,4 +40,5 @@ def user(username):
         "chat.html", username=username, chat_messages=messages)
 
 
-app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', '5000')), debug=False)
+app.run(host=os.getenv('IP', '0.0.0.0'), port=int(
+    os.getenv('PORT', '5000')), debug=False)
